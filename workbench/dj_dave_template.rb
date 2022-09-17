f3157dacc89e491f9ce8c5eac810079638b079e2
William L. Anderson <band@acm.org>
2022-09-17 16:48:23 -0500

    replace dj_dave samples; add patterns

--- workbench/dj_dave_template.rb
+++ workbench/dj_dave_template.rb
@@ -11,11 +11,20 @@ define :pattern do |pattern|
   return pattern.ring.tick == "x"
 end
 
-kick = "/Users/sarah/Desktop/dj_dave/samples/kisses/kick.wav"
-kick2 = "/Users/sarah/Desktop/dj_dave/samples/kisses/kick2.wav"
-hhc1 = "/Users/sarah/Desktop/dj_dave/samples/kisses/hhc1.wav"
-clap = "/Users/sarah/Desktop/dj_dave/samples/kisses/clap.wav"
-hhc2 = "/Users/sarah/Desktop/dj_dave/samples/kisses/hhc2.wav"
-hhc3 = "/Users/sarah/Desktop/dj_dave/samples/kisses/hhc3.wav"
-hho = "/Users/sarah/Desktop/dj_dave/samples/kisses/hho.wav"
+kick = "/Users/band/Documents//Music/Expansions Selection Library/Samples/Drums/Kick/Kick Chomp 1.wav"
+kick2 = "/Users/band/Documents//Music/Expansions Selection Library/Samples/Drums/Kick/Kick Bulldog 2.wav"
+hhc1 = "/Users/band/Documents//Music/Expansions Selection Library/Samples/Drums/Hihat/ClosedHH Graphite 1.wav"
+clap= "/Users/band/Documents/Music/Expansions Selection Library/Samples/Drums/Clap/Clap SouthTown.wav"
+hhc2 = "/Users/band/Documents//Music/Expansions Selection Library/Samples/Drums/Hihat/ClosedHH Chomp 2.wav"
+hhc3 = "/Users/band/Documents//Music/Expansions Selection Library/Samples/Drums/Hihat/ClosedHH Astral.wav"
+hho = "/Users/band/Documents//Music/Expansions Selection Library/Samples/Drums/Hihat/OpenHH Analie.wav"
 
+# some prime number patterns
+
+##| "-xx-x-x---x-x---" # 1-16
+##| "x-x---x-----x-x-" # 17-32
+##| "-----x---x-x---x-" # 33-48
+##| "----x-----x-x---" # 49-64
+##| "--x---x-x-----x-" # 65-80
+##| "--x-----x-------" # 81-96
+##| "x---x-x---x-x---" # 97-112
